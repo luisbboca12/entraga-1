bad47a8dcf6f8644467ade36ee573fd4b1049cc5
luisbboca12 <107447827+luisbboca12@users.noreply.github.com>
2023-11-30 01:49:17 -0300

    Update product.js

--- product.js
+++ product.js
@@ -35,21 +35,14 @@ class ProductManager {
     }
     return product;
   }
-
   generateId() {
-    // Se podría implementar lógica más robusta para la generación de IDs
-    // pero para este ejemplo, simplemente usaremos un timestamp
+    
     return new Date().getTime().toString();
   }
 }
-
-// Crear una instancia de ProductManager
 const productManager = new ProductManager();
-
-// Llamar a getProducts después de crear la instancia
 console.log('Productos después de crear la instancia:', productManager.getProducts());
 
-// Añadir un producto
 const productId = productManager.addProduct({
   title: 'producto prueba',
   description: 'Este es un producto prueba',
@@ -60,26 +53,26 @@ const productId = productManager.addProduct({
 });
 console.log('Producto agregado exitosamente con ID:', productId);
 
-// Llamar a getProducts después de agregar un producto
+
 console.log('Productos después de agregar un producto:', productManager.getProducts());
 
-// Intentar añadir un producto con el mismo código
+
 const duplicatedProductId = productManager.addProduct({
   title: 'producto prueba',
   description: 'Este es un producto prueba',
   price: 200,
   thumbnail: 'Sin imagen',
-  code: 'abc123', // Este código ya está en uso
+  code: 'abc123',
   stock: 25,
 });
 console.log('Producto agregado exitosamente con ID:', duplicatedProductId);
 
-// Evaluar getProductById para un producto existente
-const firstProductId = productManager.getProducts()[0].id; // Tomamos el ID del primer producto
+
+////Evaluacioooooonn
+const firstProductId = productManager.getProducts()[0].id; 
 const productById = productManager.getProductById(firstProductId);
 console.log('Producto encontrado por ID:', productById);
 
-// Evaluar getProductById para un producto inexistente
-const nonExistentProductId = 'nonexistentid';
+const nonExistentProductId = 'nonexisteXD';
 const nonExistentProductById = productManager.getProductById(nonExistentProductId);
 console.log('Producto encontrado por ID:', nonExistentProductById);
